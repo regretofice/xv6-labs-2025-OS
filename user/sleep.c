addcef18df5734390d00a165ab5ec74cf6498ad3
regretofice <1651979037@qq.com>
2026-07-14 00:31:08 +0800

    第一次尝试完成项目

---
+++ user/sleep.c
@@ -0,0 +1,12 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+// 头文件顺序不能错
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    printf("usage: sleep <time>\n");
+    return 1;
+  }
+  pause(atoi(argv[1]));
+  return 0;
+}
